7380cc9e4779d2218bf405c560c665f2d9a7e29f
apChii85 <edgars.martinsons@gmail.com>
2024-11-23 15:09:34 +0200

    MD

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print ("hello world")
+import paramiko
+# header
+header = """
+    ---OS version /"Crawler/"
+    Author: Edgars
+    Release date: 23.11.2024
+    Course: Pitors drošības testētājiem
+"""
+line_break = "."
+print (line_break * 100)
+print (header)
+print (line_break * 100)
+# define a list of servers
+
+# connect to each server
+
+# get os version
+
+# output
